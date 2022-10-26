0f4c150db66acda67a97090c9a0ef6b9c36a8212
Ingrisson Murilo dos Santos <116690288+iFerael@users.noreply.github.com>
2022-10-26 00:31:06 +0000

    Criar e Exibir variáveis

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 # Meu primeiro projeto Python!!
+#
 '''Comentário em Bloco'''
 
 # print() = comando de saída
 print("Alo Mundo!")
+
+#Quando quiser guardar uma String! (frase)
+nome = "Ingrisson Santos"
+#Aspas para alfanuméricos
+idade = 26
+
+#Exibe as variáveis
+print(nome, idade)
+
