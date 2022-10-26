c1de91f28ef242b43ccba4712cc2e98765a5a0cc
Ingrisson Murilo dos Santos <116690288+iFerael@users.noreply.github.com>
2022-10-26 23:13:20 +0000

    Comando de entrada input e diferenças str com int

---
+++ Aula1-19-10-2022-2.py
@@ -0,0 +1,18 @@
+# Meu primeiro projeto Python!!
+#
+'''Comentário em Bloco'''
+
+# print() = comando de saída
+print("Alo Mundo!")
+
+#Quando quiser guardar uma String! (frase)
+nome = "Ingrisson Santos"
+#Aspas para alfanuméricos
+idade = 26
+
+#Exibe as variáveis
+print(nome, idade)
+
+#Quando quiser exibir a frase "Minha idade é" completando com o conteúdo da variável idade
+print("Meu nome é "+nome)
+print("Minha idade é "+str(idade))
--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-# Meu primeiro projeto Python!!
-#
-'''Comentário em Bloco'''
+#Comando input(): quero permitir que
+#O usuário digite algo
+nome = str(input("Digite seu nome: ")) 
+idade = int(input("Qual sua idade? "))
+#Comando de saída, exibir na tela
+print(f"Boa noite, seu nome é {nome}")
+print(f"Sua idade é {idade} anos")
+#print(f"Sua idade é {}" .format (idade))
 
-# print() = comando de saída
-print("Alo Mundo!")
+#Se eu quiser mostrar o dobro da idade?
+dobro = idade * 2
+print("O dobro da idade informada é {}".format(dobro))
 
-#Quando quiser guardar uma String! (frase)
-nome = "Ingrisson Santos"
-#Aspas para alfanuméricos
-idade = 26
 
-#Exibe as variáveis
-print(nome, idade)
 
-#Quando quiser exibir a frase "Minha idade é" completando com o conteúdo da variável idade
-print("Meu nome é "+nome)
-print("Minha idade é "+str(idade))
